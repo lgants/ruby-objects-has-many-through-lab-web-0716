520812c1898cc243da838c84575462e34e17236b
lgants <lgants@gmail.com>
2016-08-01 16:24:37 -0400

    added notation to clarify lab -ltg

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,16 @@
 class Song
-  attr_accessor :genre, :artist  ,:name
+  #song belongs to genre and song belongs to artist
+  #artist needed because Artist class contains: song.artist = self
+  attr_accessor :genre, :artist, :name
 
+
+  #initialized with genre object
   def initialize(name, genre)
     @name = name
+    #this creates the genre instance variable accessed above
     @genre = genre
-    #associates the song to the genre passed in as an argument
+    #associates the song instance to the genre passed in as an argument
+    #this accesses the add_song method from the genre object - see Genre class
     genre.add_song(self)
   end
 end
